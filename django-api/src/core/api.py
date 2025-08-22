56718eaa2b381c9dafd419c9b79ab4ec00290edb
Nelson Fleig <nelson.fleig@rover.com>
2025-08-22 17:55:56 +0200

    scaffold project

---
+++ django-api/src/core/api.py
@@ -0,0 +1,24 @@
+from ninja import Schema
+from ninja_extra import NinjaExtraAPI
+from ninja_jwt.authentication import JWTAuth
+from ninja_jwt.controller import NinjaJWTDefaultController
+
+api = NinjaExtraAPI()
+api.register_controllers(NinjaJWTDefaultController)
+
+
+class UserSchema(Schema):
+    username: str
+    is_authenticated: bool
+    email: str | None = None
+
+
+@api.get("/hello")
+def hello(request):
+    print(request)
+    return "hello world"
+
+
+@api.get("/me", response=UserSchema, auth=JWTAuth())
+def me(request):
+    return request.user
