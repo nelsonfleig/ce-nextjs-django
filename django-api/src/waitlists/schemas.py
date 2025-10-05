878fed0c4696c2f8d996b6992b5fafdc25731f44
Nelson Fleig <nfleig@hotmail.com>
2025-10-06 00:27:18 +0200

    add waitlist app

---
+++ django-api/src/waitlists/schemas.py
@@ -0,0 +1,13 @@
+from datetime import datetime
+
+from ninja import Schema
+from pydantic import EmailStr
+
+
+class WaitlistEntryCreateSchema(Schema):
+    email: EmailStr
+
+
+class WaitlistEntryDetailSchema(Schema):
+    email: EmailStr
+    timestamp: datetime
